f258f3b2bef13dd4fabb1135042d43f88266148d
Demo Server <developers@fayebsg.com>
2019-07-23 10:45:30 -0700

    Added private dir for private data

--- restoreFromS3.sh
+++ restoreFromS3.sh
@@ -1,4 +1,7 @@
 #! /bin/sh -
+
+# vars
+bucketName="$(sh ${0/*}/private/getBucketName.sh)";
 # Restore Sugar instance from s3
 # List instace back-ups in s3
-aws s3 ls s3://fbsg-demoserver
+aws s3 ls s3://$bucketName
